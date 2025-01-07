0be912c97748473704449992c532532fd2442694
BriMelany <briyitmelanycapcha@gmail.com>
2025-01-06 22:06:01 -0500

    Ejercicio 5

---
+++ EJERCICIOS_SEM1/ejercicio5.js
@@ -0,0 +1,20 @@
+ let a=parseInt(prompt("Ingrese primer numero"))
+ let b=parseInt(prompt("Ingrese segundo numero"))
+ let c=parseInt(prompt("Ingrese el tercer numero"))
+ let d=parseInt(prompt("Ingrese el cuarto numero"))
+ let mayor=0
+
+ if ( mayor <= a){
+    mayor=a
+ }
+ if(mayor <= b){
+    mayor=b
+ }
+ if(mayor <=c){
+    mayor=c
+ }
+ if(mayor<=d){
+    mayor=d
+ }
+
+ console.log(mayor)
