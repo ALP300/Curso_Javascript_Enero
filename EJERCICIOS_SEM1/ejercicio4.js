48b252479a92be03dc36737e40d49508085ddda8
Ericson-1412 <caldito699@gmail.com>
2025-01-06 21:58:01 -0500

    Agregar Ejercicio4

---
+++ EJERCICIOS_SEM1/ejercicio4.js
@@ -0,0 +1,12 @@
+let a=parseInt(prompt("Ingrese el primer numero:"));
+let b=parseInt(prompt("Ingrese el segundo numero:"));
+
+if(a>b){
+    console.log("El primer numero es mayor");
+}
+else if(b>a){
+    console.log("El segundo numero es mayor");
+}
+else{
+    console.log("Ambos numeros son iguales");
+}
